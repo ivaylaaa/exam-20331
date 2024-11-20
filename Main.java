886dbd8ec78c72c0b93a50356c6bfb31cde21dad
dikel <dikelito@tutamail.com>
2024-11-20 23:47:45 +0200

    initial-202-commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        cat = new Cat();
+        cat.meow();
+    }
+
+    static class Cat {
+        meow() {
+            System.out.println("Meow");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # uktc-exam-20X
+
+This is exam for class 202
+
+Code contributors:
+- Deyan Dimitrov 14210
+- John Doe 20299
