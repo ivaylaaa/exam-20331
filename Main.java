1d6a061ad9366a0294d54ef9c4b3bc8ca46ca7ec
dikel <dikelito@tutamail.com>
2024-11-20 23:54:24 +0200

    initial-204-commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Mouse mouse = new Mouse;
+        mouse.squeak();
+    }
+
+    static class Mouse {
+        squeak() {
+            System.println("Squeak");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # uktc-exam-20X
+
+This is exam for class 204
+
+Code contributors:
+- Deyan Dimitrov 14210
+- John Doe 20499
